e014713cf841a1855b3d3a80fbceb1b7d30b22da
RitaPylypaka <ritapylypaka@gmail.com>
2023-10-17 15:07:55 +0300

    dasha3

---
+++ pr5.0/UnitTest5/UnitTest5.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../pr5.0/pr5.0.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest5
+{
+	TEST_CLASS(UnitTest5)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = h(2, 3);
+			Assert::AreEqual(t, 0.145796, 0.0000001);
+		}
+	};
+}
